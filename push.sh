eb805f332f887c23fcad619fe0816f8dd771e6ac
Kmayankkr <kr.mayank1997@gmail.com>
2018-02-08 19:20:09 +0530

    mayank

--- push.sh
+++ push.sh
@@ -1,7 +1,7 @@
 #!/bin/bash  
 
 git remote rm origin
-git remote add origin https://Kmayankkr:2deef9a7e01d82008346a4159dcabff233556d3e@github.com/Kmayankkr/crossmodal
+git remote add origin https://Kmayankkr:bb35ac011d0e328d3da907b8e03188ab5745279a@github.com/Kmayankkr/crossmodal
 
 git add .   
 git commit -m "mayank"  
